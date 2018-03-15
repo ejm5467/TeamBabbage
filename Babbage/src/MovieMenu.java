2915fbedf58673701c9bbd90bd892a8d5c90a071
Emily <Emily@DESKTOP-7AHUBOB.attlocal.net>
2018-03-15 11:18:39 -0500

    Initial commit. Project build with 3 sample classes.

---
+++ Babbage/src/MovieMenu.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Emily
+ */
+public class MovieMenu {
+    
+    /**
+     * This is the default constructor for the MovieMenu class.
+     */
+    public MovieMenu(){
+        
+    }
+    
+    /**
+     * Adds a movie to the database.
+     * @param movie specifies the movie
+     */
+    void addMovie(String movie){
+        String movieChoice = movie;
+    }
+}
---
+++ Babbage/src/SystemMenu.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Emily
+ */
+
+/**
+ * Displays and Controls Functions of System Menu
+ */
+public class SystemMenu {
+ 
+    
+    /**
+     * This is the default constructor for the SystemMenu class.
+     */
+    public SystemMenu(){
+    
+    }
+    
+    /**
+     * Selects the Change Product Menu.
+     */
+    public void SelectChangeProduct(){
+
+    }
+}
+
+
